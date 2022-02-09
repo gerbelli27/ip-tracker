6b8e43ced4d93bd0903afb87cd894a6e40c808a3
gerbelli27 <guerbell@hotmail.com>
2022-02-09 18:09:22 +0000

    fix errors

--- script.js
+++ script.js
@@ -1,5 +1,61 @@
-lati = "";
-long = "";
+var search = document.querySelector("#input-search");
+var value = search.value
+var ip = ""
+var api_key = "at_CB9Q0AURWleU7KQ67SOGa5XOHbUPP";
+$(function () {
+    $.ajax({
+        url: "https://geo.ipify.org/api/v1",
+        data: { apiKey: api_key, ipAddress: ip, domain: value },
+        success: function (data) {
+            ip = data.ip;
+            isp = data.isp;
+            city = data.location.city;
+            region = data.location.region;
+            country = data.location.country;
+            time = data.location.timezone;
+            document.querySelector("#ma").innerHTML = '<div id="map"> </div>';
+            document.querySelector("#out-ip").innerHTML = ip;
+            document.querySelector("#out-city").innerHTML = city;
+            document.querySelector("#out-time").innerHTML = "UTC" + time;
+            document.querySelector("#out-isp").innerHTML = isp;
+            updatePosition([data.location.lat, data.location.lng])
+        }
+    });
+});
+
+const searchDomain = (event) => {
+    event.preventDefault()
+    var search = document.querySelector("#input-search");
+    var value = search.value
+    var ip = ""
+    var api_key = "at_CB9Q0AURWleU7KQ67SOGa5XOHbUPP";
+    $(function () {
+        $.ajax({
+            url: "https://geo.ipify.org/api/v1",
+            data: { apiKey: api_key, ipAddress: ip, domain: value },
+            success: function (data) {
+                updatePosition([data.location.lat, data.location.lng])
+                ip = data.ip;
+                isp = data.isp;
+                city = data.location.city;
+                region = data.location.region;
+                country = data.location.country;
+                time = data.location.timezone;
+
+                document.querySelector("#out-ip").innerHTML = ip;
+                document.querySelector("#out-city").innerHTML = city;
+                document.querySelector("#out-time").innerHTML = "UTC" + time;
+                document.querySelector("#out-isp").innerHTML = isp;
+            }
+        });
+    });
+
+}
+
+const button = document.querySelector('#button');
+button.addEventListener('click', searchDomain)
+
+//MAP//
 
 var newIcon = L.icon({
     iconUrl: './images/icon-location.svg',
@@ -7,98 +63,16 @@ var newIcon = L.icon({
     iconAnchor: [18, 46], // point of the icon which will correspond to marker's location
 });
 
-const map = L.map('map').setView([lati, long], 13);
-var marker = L.marker([lati, long]).addTo(map);
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 22,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoiZWR1YXJkb2dlcmJlbGxpIiwiYSI6ImNremJ0dW1leDFzajcybm45YThqcmU3ZnIifQ.YdCeY7xUU8siCzO7dWBDzw'
-}).addTo(map);
-
 updatePosition = (updatePosition) => {
-    map.setView(updatePosition, 13)
-    L.marker(updatePosition, { icon: newIcon }).addTo(map);
-}
-
-// IF 
-
-
-
-
-
-
-
-
-
-//API ipify
-
-/*var ip = "";
-var api_key = "at_VbLAcOoP4YtKGUjAPYKo4e1BLJnwv";
-$(function () {
-    $.ajax({
-        url: "https://geo.ipify.org/api/v1",
-        data: { apiKey: api_key, ipAddress: ip },
-        success: function (data) {
-            updatePosition([data.location.lat, data.location.lng])
-            console.log(data)
-
-            ip = data.ip;
-            isp = data.isp;
-            city = data.location.city;
-            region = data.location.region;
-            country = data.location.country;
-            time = data.location.timezone;
-
-            document.querySelector("#out-ip").innerHTML = ip;
-            document.querySelector("#out-city").innerHTML = city;
-            document.querySelector("#out-time").innerHTML = time;
-            document.querySelector("#out-isp").innerHTML = isp;
-        }
-    });
-});*/
-
-
-
-function enviar() {
-
-    var search = document.getElementById("input-search");
-    event.preventDefault();
-    if (search.value != "") {
-        let ip = search.value;
-        let www = search.value
-        var api_key = "at_VbLAcOoP4YtKGUjAPYKo4e1BLJnwv";
-        $(function () {
-            $.ajax({
-                url: "https://geo.ipify.org/api/v1",
-                data: { apiKey: api_key, ipAddress: ip, domain: www},
-                success: function (data) {
-                    updatePosition([data.location.lat, data.location.lng])
-                    console.log(data)
-
-                    ip = data.ip;
-                    isp = data.isp;
-                    city = data.location.city;
-                    region = data.location.region;
-                    country = data.location.country;
-                    time = data.location.timezone;
-
-                    document.querySelector("#out-ip").innerHTML = ip;
-                    document.querySelector("#out-city").innerHTML = city;
-                    document.querySelector("#out-time").innerHTML = time;
-                    document.querySelector("#out-isp").innerHTML = isp;
-                }
-            });
-        });
-
-
-
-
-
-    } else {
-
-    }
-
+    document.querySelector("#ma").innerHTML = '<div id="map"> </div>';
+    const map = L.map('map').setView(updatePosition, 13);
+    var marker = L.marker(updatePosition, { icon: newIcon }).addTo(map);
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 22,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1IjoiZWR1YXJkb2dlcmJlbGxpIiwiYSI6ImNremJ0dW1leDFzajcybm45YThqcmU3ZnIifQ.YdCeY7xUU8siCzO7dWBDzw'
+    }).addTo(map);
 }
